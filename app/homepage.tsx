5b19328d9c9d41cc92039f9b4a3f1aafa5c9d790
Dylan <dylan.kid@icloud.com>
2023-12-11 13:03:03 -0500

    feat: added element to landing page

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -5,7 +5,18 @@ import styles from '../styles/homepage.module.css'
 export default function HomePage() {
     return (
         <>
-            
+
+            <div className={styles.container}>
+                <div className={styles.element}>
+                    <div className={styles.elementHeader}>
+                        <h1>ScholarCD</h1>
+                        <button className={styles.loginButton}>
+                            Log In
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
